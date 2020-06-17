84591c02a73ae34462e4fc5646eaf09dcae71cb4
RussellJamesBello <russellbello24@gmail.com>
2020-06-17 17:00:18 +0800

    Updated Routes

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,20 @@ Route::match(['get', 'post'], '/', 'AuthenticationController@login')
 		->middleware('guest');
 
 Route::middleware(['auth'])->group(function () {
-	Route::get('home', 'HomeController')
+	Route::match(['get', 'post'], 'home', 'ClientController@addNewRecord')
 			->name('home');
+
+	Route::get('clients', 'ClientController@clientList');
+
+	Route::get('clients/{client}', 'ClientController@clientInfo');
+
+	Route::match(['get', 'put'], 'clients/{client}/edit', 'ClientController@editClient');
+
+	Route::match(['get', 'put'], 'records/{record}/edit', 'RecordController@editRecord');
+
+	Route::delete('clients/{client}', 'ClientController@removeClient');
+
+	Route::delete('records/{record}', 'RecordController@removeRecord');
 
 	Route::post('logout', 'AuthenticationController@logout')->name('logout');
 });
